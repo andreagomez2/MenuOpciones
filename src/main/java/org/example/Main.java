0b529533484c18b532572b2139e580378e48c980
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-09-09 14:07:45 -0500

    cambio 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,28 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+
+        //Programa para crear un ciclo
+        //crear un menu de opciones
+        Integer opcionSeleccionada=0;
+        while (opcionSeleccionada!=5){
+            System.out.println("menu de opciones");
+            System.out.println("*****************");
+            System.out.println("➕digita 1 para sumar");
+            System.out.println("➖digita 2 para restar");
+            System.out.println("❌digita 3 para multiplicar");
+            System.out.println("➗digita 5 para dividir");
+            System.out.println("➡ digita 5 para SALIR DEL PROGRAMA");
+            Scanner leerTeclado=new Scanner(System.in);
+            System.out.println("seleccione una opcion porfavor:☺");
+            opcionSeleccionada=leerTeclado.nextInt();
+
+        }
+
     }
 }
