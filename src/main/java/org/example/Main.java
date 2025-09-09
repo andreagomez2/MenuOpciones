252b2be36cfa6f0c5c2e18ac403759b0c59f564c
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-09-09 14:37:13 -0500

    cambio 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,13 @@
 package org.example;
 
 import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         //Programa para crear un ciclo
         //crear un menu de opciones
-        Integer opcionSeleccionada=0;
-        while (opcionSeleccionada!=5){
+        Integer opcionSeleccionada = 0;
+        while (opcionSeleccionada != 5) {
             System.out.println("menu de opciones");
             System.out.println("*****************");
             System.out.println("➕digita 1 para sumar");
@@ -18,11 +15,24 @@ public class Main {
             System.out.println("❌digita 3 para multiplicar");
             System.out.println("➗digita 5 para dividir");
             System.out.println("➡ digita 5 para SALIR DEL PROGRAMA");
-            Scanner leerTeclado=new Scanner(System.in);
+            Scanner leerTeclado = new Scanner(System.in);
             System.out.println("seleccione una opcion porfavor:☺");
-            opcionSeleccionada=leerTeclado.nextInt();
+            opcionSeleccionada = leerTeclado.nextInt();
 
+            //evaluando caminos segun la opcion del usuario:
+            if (opcionSeleccionada == 1) {
+                System.out.println("sumando");
+            } else if (opcionSeleccionada == 2) {
+                System.out.println("restando..");
+            } else if (opcionSeleccionada == 3) {
+                System.out.println("multiplicando");
+            } else if (opcionSeleccionada == 4) {
+                System.out.println("dividiendo");
+            } else if (opcionSeleccionada == 5) {
+                System.out.println("saliendo..");
+            } else {
+                System.out.println("opcion invalida..");
+            }
         }
-
     }
 }
